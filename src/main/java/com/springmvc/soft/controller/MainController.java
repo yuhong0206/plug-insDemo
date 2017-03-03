102cf41caaf3bbe1c6736bfefc92df31c28d511c
unknown <>
2017-03-03 21:55:51 +0800

    我的插件demo

---
+++ src/main/java/com/springmvc/soft/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.springmvc.soft.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by 宏锅锅 on 2017/1/2.
+ */
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+    //初始化访问页面
+    @RequestMapping()
+    public String initWelcome(){
+        return "index";
+    }
+}
---
+++ src/main/webapp/WEB-INF/ftl/index.ftl
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>首页</title>
+    </head>
+
+    <body>
+        <a>dataTables表格</a>
+    </body>
+</html>
